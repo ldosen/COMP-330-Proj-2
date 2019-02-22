2bc9753e81c39268ef2132450b55205cf4ffef23
eamobi <amobisomto@gmail.com>
2019-02-21 22:31:34 -0600

    search for tweets with the api

---
+++ front-end/relTweets.js
@@ -0,0 +1,16 @@
+require('dotenv').load();
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.get('search/tweets', {q: '#eminem #beyonce'}, function(error, tweets, response) {
+   tweets.statuses.forEach(function(tweet) {
+   	console.log("tweet: " + tweet.text)
+   });
+});
