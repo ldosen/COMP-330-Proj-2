a832ce045d841e2cd7e134df413dc67a746b10b1
eamobi <amobisomto@gmail.com>
2019-02-23 18:53:18 -0600

    connected express back end to react

--- ExpressServer/front-end/src/App.js
+++ ExpressServer/front-end/src/App.js
@@ -32,6 +32,22 @@ class App extends React.Component {
     this.topicInfo.miscInfo2 = data.miscInfo2;
     this.topicInfo.miscInfo3 = data.miscInfo3;
   };
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
 
   render() {
     return (
