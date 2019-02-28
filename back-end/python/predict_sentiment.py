a48c587cb79c3571bd2ede6b51220360c1259c32
Luke Dosen <lddosen@gmail.com>
2019-02-28 17:20:21 -0600

    sentiment analysis modified to be called by the server.js file

--- back-end/python/predict_sentiment.py
+++ back-end/python/predict_sentiment.py
@@ -3,13 +3,25 @@ import re
 import pickle
 from tweetclassifier.vectorizer import vect
 import numpy as np
+import argparse
 
-clf = pickle.load(open(os.path.join('tweetclassifier', 'pkl_objects', 'classifier.pkl'), 'rb'))
 
-label = {0:'negative', 1:'positive'}
+def main(message):
+    clf = pickle.load(open(os.path.join('tweetclassifier', 'pkl_objects', 'classifier.pkl'), 'rb'))
 
-example = ['Not sure about the pokemon...']
-X = vect.transform(example)
-print('Prediction: %s\nProbability: %.2f%%' %\
-      (label[clf.predict(X)[0]], clf.predict_proba(X).max()*100))
+    label = {0: 'negative', 1: 'positive'}
+
+    example = ['I hate him']
+    X = vect.transform(example)
+    predicted_class = label[clf.predict(X)[0]]
+
+    return predicted_class
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--message", type=str)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    main(args.message)
 
