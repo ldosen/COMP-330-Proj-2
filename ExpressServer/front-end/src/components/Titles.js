cb4c49b0e877b1f15f5b79d3f4204a14bc47ab41
Jonathan <warkentin.jon@gmail.com>
2019-03-02 00:30:56 -0600

    change main description

--- ExpressServer/front-end/src/components/Titles.js
+++ ExpressServer/front-end/src/components/Titles.js
@@ -5,8 +5,8 @@ const Titles = () => (
   <div>
     <h1 id="main-title">TWEETTRENDS</h1>
     <h2 id="main-description">
-      Sentiment analyses and other information for the top 10 trending topics on
-      Twitter
+      Sentiment analyses and other information for 10 of the top trending topics
+      on Twitter
     </h2>
   </div>
 );
