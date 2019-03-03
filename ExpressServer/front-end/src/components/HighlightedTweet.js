c49d7a53772ce8bd40e4af7fd99a66b665f568a7
Jonathan <warkentin.jon@gmail.com>
2019-03-02 19:21:23 -0600

    in server make highlighted tweet messages shorten to 82 chars + ...

--- ExpressServer/front-end/src/components/HighlightedTweet.js
+++ ExpressServer/front-end/src/components/HighlightedTweet.js
@@ -3,14 +3,18 @@ import TweetEmbed from "react-tweet-embed";
 
 //Renders the message content from the most recent tweet on a given topic.
 //TweetEmbed no longer in use as it was inexplicably rendering two tweets only on page load
-const HighlightedTweet = props => (
-  <div id="highlighted-tweet-container">
-    <h3 id="highlighted-tweet-description">
-      Most recent tweet about {props.name}:
-    </h3>
+class HighlightedTweet extends React.Component {
+  render() {
+    return (
+      <div id="highlighted-tweet-container">
+        <p id="highlighted-tweet-description">
+          Most recent tweet about {this.props.name}:
+        </p>
 
-    <h2 id="highlighted-tweet">"{props.tweetMessage}"</h2>
-  </div>
-);
+        <h2 id="highlighted-tweet">"{this.props.tweetMessage}"</h2>
+      </div>
+    );
+  }
+}
 
 export default HighlightedTweet;
--- ExpressServer/server.js
+++ ExpressServer/server.js
@@ -36,12 +36,26 @@ app.get("/express_backend", (req, res) => {
   });
 });
 
+function shortenTweet(inputTweet) {
+  if (inputTweet === undefined) {
+    var resultTweet = inputTweet;
+  } else if (inputTweet.length > 85) {
+    var resultTweet = inputTweet.substring(0, 82);
+    resultTweet = resultTweet + "...";
+  } else {
+    var resultTweet = inputTweet;
+  }
+  return resultTweet;
+}
+
 // dummy info
 const allInfo = [
   {
     name: "Cohen",
     sentiment: "negative",
-    tweetMessage: "testing1",
+    tweetMessage: shortenTweet(
+      "loremEnim occaecat aliqua aliqua exercitation sint nostrud. Consectetur sunt non eiusmod nostrud dolor tempor voluptate irure commodo labore sint. Labore ut tempor consequat cillum ex aute sit veniam ad excepteur veniam sunt anim. ComUllamco culpa commodo officia consequat culpa eu quis anim aute enim veniam. Sint esse nostrud aliquip consequat sit occaecat consequat proident veniam labore ipsum. Aute fugiat nostrud dolor deserunt qui"
+    ),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [20, 18, 12, 5, 10, 12, 4]
@@ -51,7 +65,7 @@ const allInfo = [
   {
     name: "Jim Jordan",
     sentiment: "negative",
-    tweetMessage: "testing2",
+    tweetMessage: shortenTweet("testing2"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [, 25, 20, 10, 15, 11, 12]
@@ -61,7 +75,7 @@ const allInfo = [
   {
     name: "#PokemonSwordShield",
     sentiment: "positive",
-    tweetMessage: "testing3",
+    tweetMessage: shortenTweet("testing3"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [undefined, undefined, undefined, undefined, undefined, 90, 95]
@@ -71,7 +85,7 @@ const allInfo = [
   {
     name: "#PokemonDirect",
     sentiment: "positive",
-    tweetMessage: "testing4",
+    tweetMessage: shortenTweet("testing4"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [undefined, undefined, undefined, undefined, undefined, 91, 94]
@@ -81,7 +95,7 @@ const allInfo = [
   {
     name: "Scorbunny",
     sentiment: "positive",
-    tweetMessage: "testing5",
+    tweetMessage: shortenTweet("testing5"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [undefined, undefined, undefined, undefined, undefined, 92, 98]
@@ -91,7 +105,7 @@ const allInfo = [
   {
     name: "Grookey",
     sentiment: "positive",
-    tweetMessage: "testing6",
+    tweetMessage: shortenTweet("testing6"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [undefined, undefined, undefined, undefined, undefined, 97, 91]
@@ -101,7 +115,7 @@ const allInfo = [
   {
     name: "Sobble",
     sentiment: "positive",
-    tweetMessage: "testing7",
+    tweetMessage: shortenTweet("testing7"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [undefined, undefined, undefined, undefined, undefined, 96, 93]
@@ -111,7 +125,7 @@ const allInfo = [
   {
     name: "Mark Meadows",
     sentiment: "negative",
-    tweetMessage: "testing8",
+    tweetMessage: shortenTweet("testing8"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [40, 38, 30, 35, 20, 15, 12]
@@ -121,7 +135,7 @@ const allInfo = [
   {
     name: "#PokemonDay",
     sentiment: "positive",
-    tweetMessage: "testing9",
+    tweetMessage: shortenTweet("testing9"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [85, 83, 80, 90, 95, 93, 94]
@@ -131,7 +145,7 @@ const allInfo = [
   {
     name: "Pokémon",
     sentiment: "positive",
-    tweetMessage: "testing10",
+    tweetMessage: shortenTweet("testing10"),
     graph: {
       xData: ["2/21", "2/22", "2/23", "2/24", "2/25", "2/26", "2/27"],
       yData: [85, 83, 82, 85, 90, 91, 92]
