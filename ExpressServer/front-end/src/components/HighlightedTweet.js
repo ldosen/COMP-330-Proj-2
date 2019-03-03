167405c2afd5ab416f04e7f1ed2016188be418e8
Jonathan <warkentin.jon@gmail.com>
2019-03-02 20:05:52 -0600

    change HighlightedTweet back into stateless functional component

--- ExpressServer/front-end/src/components/HighlightedTweet.js
+++ ExpressServer/front-end/src/components/HighlightedTweet.js
@@ -1,20 +1,15 @@
 import React from "react";
 
 //Renders the message content from the most recent tweet on a given topic.
-//TweetEmbed no longer in use as it was inexplicably rendering two tweets only on page load
-class HighlightedTweet extends React.Component {
-  render() {
-    return (
-      <div id="highlighted-tweet-container">
-        <h3 id="highlighted-tweet-description">
-          Most recent tweet about{" "}
-          <span id="highlighted-tweet-topic-name">{this.props.name}</span> :
-        </h3>
+const HighlightedTweet = props => (
+  <div id="highlighted-tweet-container">
+    <h3 id="highlighted-tweet-description">
+      Most recent tweet about{" "}
+      <span id="highlighted-tweet-topic-name">{props.name}</span> :
+    </h3>
 
-        <h2 id="highlighted-tweet">"{this.props.tweetMessage}"</h2>
-      </div>
-    );
-  }
-}
+    <h2 id="highlighted-tweet">"{props.tweetMessage}"</h2>
+  </div>
+);
 
 export default HighlightedTweet;
